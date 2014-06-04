a2a6983371c82339c741100ffe07bba5c1b6e166
Boris Filipov <obiwonn@gmail.com>
2014-06-04 23:10:49 +0300

    switch to use internal plasma reference as Organel core impl. passes in sender value which is not compatible with organic-plasma

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ module.exports = Organel.extend(function(plasma, dna){
     glob.create(options.root+options.pattern)
       .on("data", function(file){
         if(options.forEachEmit)
-          self.emit(_.extend({
+          self.plasma.emit(_.extend({
             data: file,
             root: options.root
           }, options.forEachEmit))
       })
       .on("error", function(err){
         if(options.emitReady)
-          self.emit({type: options.emitReady, err: err})
+          self.plasma.emit({type: options.emitReady, err: err})
         if(next)
           next(err)
       })
       .on("end", function(){
         if(options.emitReady)
-          self.emit({type: options.emitReady, err: null})
+          self.plasma.emit({type: options.emitReady, err: null})
         if(next)
           next()
       })
